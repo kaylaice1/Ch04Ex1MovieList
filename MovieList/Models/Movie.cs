fe0a16ca21ec8e86c50578d69b0eb4406723e566
KayLynn Johnson <johnson.kaylynn@outlook.com>
2024-10-06 12:22:45 -0500

    step 29

--- MovieList/Models/Movie.cs
+++ MovieList/Models/Movie.cs
@@ -4,10 +4,11 @@ namespace MovieList.Models
 {
     public class Movie
     {
+        // EF Core will configure the database to generate this value
         public int MovieId { get; set; }
 
         [Required(ErrorMessage = "Please enter a name.")]
-        public string? Name { get; set; }
+        public string Name { get; set; }
 
         [Required(ErrorMessage = "Please enter a year.")]
         [Range(1889, 2999, ErrorMessage = "Year must be after 1889.")]
@@ -16,5 +17,12 @@ namespace MovieList.Models
         [Required(ErrorMessage = "Please enter a rating.")]
         [Range(1, 5, ErrorMessage = "Rating must be between 1 and 5.")]
         public int? Rating { get; set; }
+
+        [Required(ErrorMessage = "Please enter a genre.")]
+        public string GenreId { get; set; }
+        public Genre Genre { get; set; }
+
+        public string Slug =>
+            Name?.Replace(' ', '-').ToLower() + '-' + Year?.ToString();
     }
 }
