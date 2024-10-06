a6a86fb902bb69af2a7766c5d2157d2697edaa05
KayLynn Johnson <johnson.kaylynn@outlook.com>
2024-10-06 11:55:53 -0500

    step 16

--- MovieList/Program.cs
+++ MovieList/Program.cs
@@ -1,8 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using MovieList.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-
+builder.Services.AddDbContext<MovieContext>(options =>
+{
+    //The name of the connection string is taken from appsetting.json under ConnectionStrings
+    options.UseSqlServer(builder.Configuration.GetConnectionString("MovieContext"));
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
